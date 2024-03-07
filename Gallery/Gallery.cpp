4a3093ab7a2f361b82566ea17e6ace6cede81037
Itay Dali <itaydali8@gmail.com>
2024-03-07 15:00:21 +0200

    moved welcome print to another function

--- Gallery/Gallery.cpp
+++ Gallery/Gallery.cpp
@@ -10,11 +10,11 @@ int getCommandNumberFromUser()
 {
 	std::string message("\nPlease enter any command(use number): ");
 	std::string numericStr("0123456789");
-	
+
 	std::cout << message << std::endl;
 	std::string input;
 	std::getline(std::cin, input);
-	
+
 	while (std::cin.fail() || std::cin.eof() || input.find_first_not_of(numericStr) != std::string::npos) {
 
 		std::cout << "Please enter a number only!" << std::endl;
@@ -26,8 +26,17 @@ int getCommandNumberFromUser()
 		std::cout << std::endl << message << std::endl;
 		std::getline(std::cin, input);
 	}
-	
+
 	return std::atoi(input.c_str());
+}
+
+void printWelcome()
+{
+	auto timenow = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+
+	std::cout << "Gallery project by Itay Dali" << std::endl << ctime(&timenow);
+	std::cout << "===================" << std::endl;
+	std::cout << "Type " << HELP << " to a list of all supported commands" << std::endl;
 }
 
 int main(void)
@@ -40,22 +49,19 @@ int main(void)
 
 
 	std::string albumName;
-	std::cout << "Gallery project by Itay Dali" << std::endl;
-	auto timenow =	std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-	std::cout << ctime(&timenow);
-	std::cout << "===================" << std::endl;
-	std::cout << "Type " << HELP << " to a list of all supported commands" << std::endl;
-	
+
+	printWelcome();
+
 	do {
 		int commandNumber = getCommandNumberFromUser();
-		
-		try	{
+
+		try {
 			albumManager.executeCommand(static_cast<CommandType>(commandNumber));
-		} catch (std::exception& e) {	
+		}
+		catch (std::exception& e) {
 			std::cout << e.what() << std::endl;
 		}
-	} 
-	while (true);
+	} while (true);
 }
 
 
